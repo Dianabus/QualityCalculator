9547f3c858232a0a274e4a716dc89a07411ed62c
Dianabus <dianabus@op.pl>
2019-02-10 19:47:29 +0100

    add average calculation functionality

--- app/src/main/java/pl/edu/agh/qualitycalculator/CalculationsVerbalizer.java
+++ app/src/main/java/pl/edu/agh/qualitycalculator/CalculationsVerbalizer.java
@@ -2,11 +2,22 @@ package pl.edu.agh.qualitycalculator;
 
 public class CalculationsVerbalizer {
 
-    public String verbalize(Operation operation, float operand1, float operand2, float result) {
+   /* public String verbalize(Operation operation, float operand1, float operand2, float result) {
         String verbalizedOperation = String.valueOf(operand1);
         verbalizedOperation += " " + operation.toString() + " ";
         verbalizedOperation += String.valueOf(operand2);
         verbalizedOperation += " gives value ";
+        verbalizedOperation += String.valueOf(result);
+        return verbalizedOperation;
+    }*/
+
+    public String verbalize(Operation operation, float operand1, float operand2, float operand3,float result){
+        String verbalizedOperation = "Average of " + String.valueOf(operand1);
+        verbalizedOperation += "  and ";
+        verbalizedOperation += String.valueOf(operand2);
+        verbalizedOperation += "  and ";
+        verbalizedOperation += String.valueOf(operand3);
+        verbalizedOperation += "  = ";
         verbalizedOperation += String.valueOf(result);
         return verbalizedOperation;
     }
--- app/src/test/java/pl/edu/agh/qualitycalculator/CalculationsTest.java
+++ app/src/test/java/pl/edu/agh/qualitycalculator/CalculationsTest.java
@@ -16,25 +16,27 @@ public class CalculationsTest {
 
     @Test
     public void testSum() throws Exception {
-        assertEquals(5, calculationsUnderTest.calculate(Operation.SUM, 2, 3), 0.01);
+        assertEquals(5, calculationsUnderTest.calculate(Operation.SUM, 2, 3, 0), 0.01);
     }
 
     @Test
     public void testSubtract()throws Exception{
-        assertEquals(11f, calculationsUnderTest.calculate(Operation.SUBTRACT,1000, 989));
+        assertEquals(11f, calculationsUnderTest.calculate(Operation.SUBTRACT,1000, 989, 0), 0.001);
     }
 
     @Test
     public void testMult() throws Exception {
-        assertEquals(35f, calculationsUnderTest.calculate(Operation.MULTIPLY, 5, 7));
+        assertEquals(35f, calculationsUnderTest.calculate(Operation.MULTIPLY, 5, 7, 1));
     }
 
     @Test
     public void testDivide() throws Exception {
-        assertEquals(4f, calculationsUnderTest.calculate(Operation.DIVIDE, 8, 2));
+        assertEquals(4f, calculationsUnderTest.calculate(Operation.DIVIDE, 8, 2, 1));
     }
 
-
+    public void testCalcAvg() throws Exception{
+        assertEquals(2.0, calculationsUnderTest.calculate(Operation.AVERAGE,1,4,1), 0.001);
+    }
 
     @After
     public void tearDown() {
